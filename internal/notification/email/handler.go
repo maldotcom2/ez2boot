1e7e16022fd993312a994c69654fcb0fe208ea12
maldotcom2 <malcolmmalcolm_19@hotmail.com>
2025-11-06 22:42:12 +1100

    Add standardised reponse for email handler

--- internal/notification/email/handler.go
+++ internal/notification/email/handler.go
@@ -3,6 +3,7 @@ package email
 import (
 	"encoding/json"
 	"ez2boot/internal/contextkey"
+	"ez2boot/internal/shared"
 	"net/http"
 )
 
@@ -11,21 +12,26 @@ func (h *Handler) AddOrUpdate() http.HandlerFunc {
 		userID, ok := r.Context().Value(contextkey.UserIDKey).(int64)
 		if !ok {
 			h.Logger.Error("User ID not found in context")
-			http.Error(w, "User ID not found in context", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(shared.ApiResponse[any]{Success: false, Error: "User ID not found in context"})
 			return
 		}
 
 		var c Config
 		if err := json.NewDecoder(r.Body).Decode(&c); err != nil {
-			h.Logger.Error("Invalid request")
-			http.Error(w, "Invalid request", http.StatusBadRequest)
+			h.Logger.Error("Malformed request")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(shared.ApiResponse[any]{Success: false, Error: "Malformed request"})
 			return
 		}
 
 		if err := h.Service.AddOrUpdate(userID, c); err != nil {
 			h.Logger.Error("Failed to add or update email notifications", "userID", userID, "error", err)
-			http.Error(w, "Failed to create new session", http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(shared.ApiResponse[any]{Success: false, Error: "Failed to create new session"})
 			return
 		}
+
+		json.NewEncoder(w).Encode(shared.ApiResponse[any]{Success: true})
 	}
 }
