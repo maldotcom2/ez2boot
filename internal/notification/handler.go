a93453279019d51ef90e0800d91bde0a44c977b5
maldotcom2 <malcolmmalcolm_19@hotmail.com>
2025-11-06 22:50:33 +1100

    Forgot one

--- internal/notification/handler.go
+++ internal/notification/handler.go
@@ -2,16 +2,17 @@ package notification
 
 import (
 	"encoding/json"
+	"ez2boot/internal/shared"
 	"log"
 	"net/http"
 )
 
 func GetNotificationTypes() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		sender, ok := GetSender("email")
+		sender, ok := GetSender("email") // Testing
 		log.Print(ok)
 		if ok {
-			json.NewEncoder(w).Encode(sender)
+			json.NewEncoder(w).Encode(shared.ApiResponse[any]{Success: true, Data: sender})
 		}
 	}
 }
