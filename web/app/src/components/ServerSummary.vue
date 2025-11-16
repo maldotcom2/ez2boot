f722db0fbb8f5225b71c5c22b380d6c7e8ad88ce
maldotcom2 <malcolmmalcolm_19@hotmail.com>
2025-11-16 17:19:10 +1100

    UI/UX fixes on server summary

--- web/app/src/components/ServerSummary.vue
+++ web/app/src/components/ServerSummary.vue
@@ -8,6 +8,7 @@
         <tr>
           <th>Group Name</th>
           <th>Server Count</th>
+          <th>Time Remaining</th>
           <th>Expiry</th>
           <th>Current User</th>
           <th>Action</th>
@@ -17,22 +18,23 @@
         <tr v-for="server in servers" :key="server.server_group">
           <td>{{ server.server_group }}</td>
           <td><span :title="server.server_names">{{ server.server_count }}</span></td>
+          <td>{{ server.expiry ? formatTimeRemaining(Math.floor((server.expiry - Math.floor(Date.now() / 1000)) / 60)): '-' }}</td>
           <td>{{ server.expiry ? new Date(server.expiry * 1000).toLocaleString() : '-' }}</td>
           <td>{{ server.current_user || '-' }}</td>
           <td>
             <div class="controls-container">
-              <input v-model="duration" placeholder="eg 3h" :disabled="server.current_user && server.current_user !== userState.email" />
-                <!-- Nobody using session -->
-                <button v-if="!server.current_user" @click="startServerSession(server.server_group, duration)">Start Session</button>
+              <input v-model="duration[server.server_group]" placeholder="eg 3h" :disabled="server.current_user && server.current_user !== userState.email" />
+                <!-- Start Session enabled if nobody is using it -->
+              <button @click="startServerSession(server.server_group)"
+              :disabled="!!server.current_user || !duration[server.server_group]">Start Session</button>
 
-                <!-- Current session is mine -->
-                <template v-else-if="server.current_user === userState.email">
-                  <button @click="updateServerSession(server.server_group, duration)">Extend Session</button>
-                  <button @click="updateServerSession(server.server_group, '0m')">End Session</button>
-                </template>
+              <!-- Extend Session enabled for current user -->
+              <button @click="updateServerSession(server.server_group)"
+              :disabled="server.current_user !== userState.email || !duration[server.server_group]">Update Session</button>
 
-                <!-- Session is someone else -->
-                <button v-else disabled>In Use</button>
+              <!-- End Session enabled for current user-->
+              <button @click="endServerSession(server.server_group)"
+              :disabled="server.current_user !== userState.email">End Session</button>
             </div>
           </td>
         </tr>
@@ -46,7 +48,8 @@ import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import { userState } from '@/user.js'
 
-const servers = ref([]) // Makes 'servers' reactive
+const servers = ref([])
+const duration = ref({})
 
 // Load table data from specialised endpoint
 async function loadServerSessions() {
@@ -55,20 +58,27 @@ async function loadServerSessions() {
     if (response.data.success) {
         servers.value = response.data.data
     }
+    duration.value = {} // empty the input
+
     } catch (err) {
     console.error('Error loading server sessions:', err)
   }
 }
 
 // Start a new server session
-async function startServerSession(serverGroup, duration) {
+async function startServerSession(serverGroup) {
+  if (!validateDuration(duration.value[serverGroup])) {
+    return
+  }
+
   try {
     const response = await axios.post('/ui/session/new', {
       server_group: serverGroup,
-      duration: duration
+      duration: duration.value[serverGroup]
     })
 
     if (response.data.success) {
+      duration.value[serverGroup] = ''
       loadServerSessions() // refresh table after creating session
     }
   } catch (err) {
@@ -77,11 +87,15 @@ async function startServerSession(serverGroup, duration) {
 }
 
 // Update server session
-async function updateServerSession(serverGroup, duration) {
+async function updateServerSession(serverGroup) {
+  if (!validateDuration(duration.value[serverGroup])) {
+    return
+  }
+
   try {
     const response = await axios.put('/ui/session/update', {
       server_group: serverGroup,
-      duration: duration
+      duration: duration.value[serverGroup]
     })
 
     if (response.data.success) {
@@ -90,6 +104,38 @@ async function updateServerSession(serverGroup, duration) {
   } catch (err) {
     console.error('Error updating server session:', err)
   }
+}
+
+async function endServerSession(serverGroup) {
+  try {
+    const response = await axios.put('/ui/session/update', {
+      server_group: serverGroup,
+      duration: '0m'
+    })
+
+    if (response.data.success) {
+      loadServerSessions() // refresh table after creating session
+    }
+  } catch (err) {
+    console.error('Error updating server session:', err)
+  }
+}
+
+function formatTimeRemaining(minutesRemaining) {
+  if (minutesRemaining <= 1) {
+    return '< 1 minute'
+  }
+
+  else if (minutesRemaining < 0) {
+    return 'expired'
+  }
+
+  else return `${minutesRemaining} minutes`
+}
+
+function validateDuration(duration) {
+  const regex = /^\d+(m|h)$/
+  return regex.test(duration) // true
 }
 
 // Load table on page load
@@ -123,4 +169,13 @@ onMounted(() => {
   display: flex;
   gap: 10px;
 }
+
+.controls-container button {
+  width: 140px;
+}
+
+.controls-container input {
+  width: 50px;
+}
+
 </style>
